d7df1b1bc9c7adc5eda2be6571e81a9e8df725b5
lam gia nguyen <lamnguyen13112000@gmail.com>
2023-01-02 22:13:44 +0700

    add SiteRepo
    CURD SiteInformation

---
+++ BetterHealth2023.Repository/Repositories/ImplementedRepository/SiteRepos/ISiteRepo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using BetterHealthManagementAPI.BetterHealth2023.Repository.DatabaseModels;
+using BetterHealthManagementAPI.BetterHealth2023.Repository.Repositories.GenericRepository;
+
+namespace BetterHealthManagementAPI.BetterHealth2023.Repository.Repositories.ImplementedRepository.SiteRepos
+{
+    public interface ISiteRepo: IRepository<SiteInformation>
+    {
+        public Task<SiteInformation> InsertNewSite(SiteInformation siteInformation);
+        public Task<SiteInformation> UpdateSite(SiteInformation siteInformation);
+        public Task DeleteSite(SiteInformation siteInformation);
+        public Task<SiteInformation> GetSiteById(string id);
+    }
+}
