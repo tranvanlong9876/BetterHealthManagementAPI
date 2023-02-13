5ff937463c50ba1acb676bedb98abd33383eb91d
Trần Văn Long <traanvanlongo@gmail.com>
2023-02-13 17:25:25 +0700

    Create OrderController.cs

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BetterHealthManagementAPI.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class OrderController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize(Roles = "Pharmacist")]
+        public async Task<IActionResult> GetAllOrders()
+        {
+            return Ok("Có cc");
+        }
+    }
+}
